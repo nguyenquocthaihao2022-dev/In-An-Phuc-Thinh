b16ac7725e4365af3d7511e9f7732728fdcae3fa
Thái Hào <nguyenquocthaihao2022@gmail.com>
2026-07-09 20:56:26 +0700

    lưu tên và số điện thoại lên localstorage khi liên hệ, sửa lỗi hiển thị nhỏ, thêm trang giới thiệu

--- Scripts/lien_he_dat_hang.js
+++ Scripts/lien_he_dat_hang.js
@@ -3,8 +3,9 @@ if (localStorage.key("name") && localStorage.key("phone_number")) {
     document.getElementById("name_input").value = JSON.parse(localStorage.getItem("name"));
     document.getElementById("phone_number_input").value = JSON.parse(localStorage.getItem("phone_number"));
 }
-contact.addEventListener("submit", function(event) {
+contact.addEventListener("click", function(event) {
     event.preventDefault();
+    console.log("yes")
     let name = document.getElementById("name_input").value;
     let phone_number = document.getElementById("phone_number_input").value;
     localStorage.setItem("name", JSON.stringify(name));
